f92cce01301ed4678ba6b924893e84ff38b238d2
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2024-09-08 12:45:21 +0300

    Add some docs

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,20 @@ import {
 import { createRandomNameGenerator } from "./name";
 
 const lastNameGenerator = createRandomNameGenerator(getLastnameDataset());
+
+/**
+ *
+ * @returns A random weighted last name from Finnish open data
+ */
 export function lastName(): string {
   return lastNameGenerator();
 }
 
+/**
+ *
+ * @param options
+ * @returns a random weighted first name from Finnish open data
+ */
 export function firstname(options: FirstNameOptions): string {
   const finalOptions = {
     ...options,
