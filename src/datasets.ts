c0233a4228e33a80eeee53ccda063740c3b94159
Mikko Forsström <mikko.forsstrom@veikkaus.fi>
2024-09-08 12:48:05 +0300

    With

--- src/datasets.ts
+++ src/datasets.ts
@@ -1,10 +1,10 @@
-import menAll from "./data/miehet-kaikki.json" assert { type: "json" };
-import menFirst from "./data/miehet-ens.json" assert { type: "json" };
+import menAll from "./data/miehet-kaikki.json" with { type: "json" };
+import menFirst from "./data/miehet-ens.json" with { type: "json" };
 
-import womenAll from "./data/naiset-kaikki.json" assert { type: "json" };
-import womenFirst from "./data/naiset-ens.json" assert { type: "json" };
+import womenAll from "./data/naiset-kaikki.json" with { type: "json" };
+import womenFirst from "./data/naiset-ens.json" with { type: "json" };
 
-import lastNames from "./data/sukunimet.json" assert { type: "json" };
+import lastNames from "./data/sukunimet.json" with { type: "json" };
 import { NameData } from "./name";
 
 export type FirstNameOptions = {
